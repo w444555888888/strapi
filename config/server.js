f5b53190acddfa1081e3bec7db33ccbca44a4354
w444555888 <w444555888@yahoo.com.tw>
2024-06-12 23:35:48 +0800

    [update]token
    [update]token

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['yourGeneratedAppKeyA', 'yourGeneratedAppKeyB']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
